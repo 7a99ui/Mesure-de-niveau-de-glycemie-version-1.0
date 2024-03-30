2c3ed2d2ca01fc4f411b1df467b1840c88d67fd3
Haggui Akram <akramhaggui2@gmail.com>
2024-03-31 00:46:57 +0100

    fisrt_version

---
+++ Haggui_Akram_mesure_Glycemie/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Haggui_Akram_mesure_Glycemie"
+include(":app")
+ 
